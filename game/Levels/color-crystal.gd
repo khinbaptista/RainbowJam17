419a3c45f21568dae095d5efdca99854bcf34d03
Khin Baptista <khin.baptista@gmail.com>
2017-08-28 10:13:45 -0300

    Added collision area to crystal

--- game/Levels/color-crystal.gd
+++ game/Levels/color-crystal.gd
@@ -1,9 +1,14 @@
 extends Node
 
-export(int, FLAGS, "None", "Red", "Orange", "Yellow", "Green", "Blue", "Violet") var color = 0
+export(int, FLAGS, "None", "Red", "Orange", "Yellow", "Green", "Blue", "Violet") var color = 1
 
 export(NodePath) var player_path = @""
 onready var player = get_node(player_path)
 
 func activate():
+	var sprite = get_node("Sprite")
+	if sprite.has_method("play"):
+		sprite.play("break")
+		yield(sprite, "finished")
 	player.learn_color(color)
+	#queue_free()
