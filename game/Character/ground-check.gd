6a1ef9648b544ef4b7231bc7b3437fae0d7cb7d1
Khin Baptista <khin.baptista@gmail.com>
2017-08-28 20:49:55 -0300

    Beams work completely, TODO: learn new colors

--- game/Character/ground-check.gd
+++ game/Character/ground-check.gd
@@ -7,5 +7,34 @@ func _ready():
 
 func _fixed_process(delta):
 	var areas = get_overlapping_areas()
+	var beam = null
+	var ground = []
+	
 	for area in areas:
-		pass
+		if area.get_name() == "beam_area" and not beam:
+			beam = area.get_parent()
+		elif area.is_in_group("platform") or area.is_in_group("ground"):
+			ground.append(area)
+	
+	if ground.empty():
+		player.grounded = false
+		return
+	
+	var grounded = false
+	for tile in ground:
+		if not tile.has_node("sprite"): continue
+		var ground_mask = tile.get_node("sprite").get_light_mask()
+		
+		if tile.get_name() != "island":
+			printt(tile.get_name(), ground_mask)
+		
+		if ground_mask >= 2:	# has a color
+			if beam != null and ground_mask & beam.beam_color:
+				print(beam.get_name())
+				grounded = true
+				break
+		else:
+			grounded = true		# ground has no color
+			break
+	
+	player.grounded = grounded
