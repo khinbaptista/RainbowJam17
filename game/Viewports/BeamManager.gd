98f440e798c84c8b20cdbb6bee31863738d814eb
Marcelo Vasques <msouvas@gmail.com>
2017-08-29 00:00:44 -0300

    Gambiarra to solve stuff

--- game/Viewports/BeamManager.gd
+++ game/Viewports/BeamManager.gd
@@ -8,12 +8,20 @@ export var speed = -100
 export var offset = 217
 var camera
 var viewportSize
+var red_pos
+var red_beam
 
 func _ready():
 	beams = get_children()
 	camera = player.get_node("Camera2D")
 	viewportSize = get_viewport().get_rect().size
 	offset *= get_scale().x
+	
+	for beam in beams:
+		if beam.check_color() == 2:
+			red_pos = beam.get_global_pos()
+			red_beam = beam
+		beam.distance_to_red = beam.get_global_pos().x - red_pos.x
 	
 	set_process(true)
 	MoveBeansInit()
@@ -24,8 +32,13 @@ func _process(delta):
 		var beam_pos = beam.get_global_pos()
 		var beam_new_pos
 		var camera_center = camera.get_camera_screen_center()
+		
 		if(beam_pos.x < camera_center.x - viewportSize.x / 2 - offset):
-			beam_new_pos = Vector2(camera_center.x + viewportSize.x / 2 + offset, camera_center.y)
+			if beam.check_color() == 2:
+				beam_new_pos = Vector2(camera_center.x + viewportSize.x / 2 + offset, camera_center.y)
+				red_pos = beam_new_pos
+			else:
+				beam_new_pos = Vector2(red_beam.get_global_pos().x + beam.distance_to_red, camera_center.y)
 		else:
 			beam_new_pos = Vector2(beam_pos.x + (speed * delta), camera_center.y)
 		beam.set_global_pos(beam_new_pos)
--- game/Viewports/beam.gd
+++ game/Viewports/beam.gd
@@ -3,6 +3,8 @@ extends Light2D
 
 export(int, FLAGS, "None", "Red", "Orange", "Yellow", "Green", "Blue", "Violet") var beam_color = 1 setget changed_color
 export(Color, RGBA) var color_value setget changed_color_value
+var distance_to_red = 0.0
+
 
 func _ready():
 	changed_color(beam_color)
