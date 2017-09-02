ec79c886e2dca2d015f1d0a2fc65bfe84eb1b274
Khin Baptista <khin.baptista@gmail.com>
2017-09-02 03:27:14 -0300

    Crystals work! Make sure to set their script variables

--- game/Crystal/color-crystal.gd
+++ game/Crystal/color-crystal.gd
@@ -1,4 +1,4 @@
-extends Node
+extends StaticBody2D
 
 export(int, FLAGS, "None", "Red", "Orange", "Yellow", "Green", "Blue", "Violet") var color = 1
 
@@ -6,9 +6,8 @@ export(NodePath) var player_path = @""
 onready var player = get_node(player_path)
 
 func interaction():
-	var sprite = get_node("Sprite")
-	if sprite.has_method("play"):
-		sprite.play("break")
-		yield(sprite, "finished")
+#	var sprite = get_node("sprite");if sprite.has_method("play"):sprite.play("break");yield(sprite, "finished")
 	player.learn_color(color)
-	#queue_free()
+	
+#	var scale = get_scale() * 0.5; set_scale(scale)
+	queue_free()
