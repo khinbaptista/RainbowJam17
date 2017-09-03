5301560d8ef31b82783a2b2743bfc29b43148851
ViniciusMeirelles <vinicius.meirelles@outlook.com.br>
2017-09-03 04:09:52 -0300

    Slight changes to player movement + redLevel checkpoints fixed. There are problems with character movement.

--- game/Levels/001/RedLevel.gd
+++ game/Levels/001/RedLevel.gd
@@ -11,3 +11,5 @@ func _ready():
 func _on_Checkpoint_checkpoint_activated():
 	yield(get_node("platforms/tempPlatforms/platform13/sprite"), "finished")
 	get_node("platforms/tempPlatforms").queue_free()
+	get_node("Checkpoint1").enabled = true
+	get_node("Checkpoint3").enabled = true
