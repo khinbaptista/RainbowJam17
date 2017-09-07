5c306473e0bda73f493134db3f84a86ae2525531
ViniciusMeirelles <vinicius.meirelles@outlook.com.br>
2017-09-06 21:52:20 -0300

    Animations, the remake! Now with a state machine!

--- game/Character/player.gd
+++ game/Character/player.gd
@@ -18,14 +18,18 @@ export var fall_height = 50
 export var fall_speed = 350
 
 var lastCheckpoint = Vector2(0, 0)	# location to respawn
-var coins = 0
 var lastDash = 0.0 # timer to control interval between dashes
+
+var coins = 0
 
 var sound
 
 export(int, FLAGS, "None", "Red", "Orange", "Yellow", "Green", "Blue", "Purple") var colors_learned = 1
 signal new_color_learned(color)
 signal death
+
+var animState = "idle" # the state of the animation state machine
+var moveDir = "" # the direction in which the player is moving (animation)
 
 func _ready():
 	set_process(true)
@@ -40,6 +44,7 @@ func _ready():
 	advertise_colors()
 	
 func _process(delta):
+	print(get_node("Sprite").get_animation().basename())
 	if dead:
 		if get_pos().y < bufferY + fall_height and not grounded:
 			set_pos(Vector2(get_pos().x, get_pos().y+fall_speed*delta))
@@ -52,31 +57,56 @@ func _process(delta):
 		if not grounded: get_node("shadow").hide()
 		else: get_node("shadow").show()
 		
-		#if lastDash > 0: lastDash -= delta
-		#else: canDash = true
-
-func _input(event):
-	if can_move:
-		input_dash(event)
+		if lastDash > 0: lastDash -= delta
+		else:	can_move = true
 
 func input_movement(delta):
 	var movement = Vector2()
+	var hasMoved = false
 	
-	if Input.is_action_pressed("move_up"):
-		movement.y -= 1
-	if Input.is_action_pressed("move_down"):
-		movement.y += 1
-	if Input.is_action_pressed("move_left"):
-		movement.x -= 1
-	if Input.is_action_pressed("move_right"):
-		movement.x += 1
 	if Input.is_action_pressed("exit"):
 		get_node("/root/loader").change_scene("res://Menu/start_menu.tscn")
-
-	movement = check_controller_input(movement.normalized())
-
-	if not dashing and not dead:
-		apply_movement(movement, delta)
+	if not dashing and not dead and can_move:
+		if Input.is_action_pressed("move_up"):
+			moveDir = "up"
+			get_node("Sprite").set_flip_h(false)
+			movement.y -= 1
+			hasMoved = true
+		if Input.is_action_pressed("move_down"):
+			moveDir = "down"
+			get_node("Sprite").set_flip_h(false)
+			movement.y += 1
+			hasMoved = true
+		if Input.is_action_pressed("move_left"):
+			moveDir = "right"
+			get_node("Sprite").set_flip_h(true)
+			movement.x -= 1
+			hasMoved = true
+		if Input.is_action_pressed("move_right"):
+			moveDir = "right"
+			get_node("Sprite").set_flip_h(false)
+			movement.x += 1
+			hasMoved = true
+		
+		if !grounded:	animState = "death"
+		elif Input.is_action_pressed("dash") and canDash:
+			dash(self.get_travel().normalized())
+			animState = "dash"
+		elif animState == "runStart" and get_node("Sprite").get_frame()==2:	animState = "runLoop"
+		elif hasMoved:
+			get_node("timer_idle").stop()
+			canDash = true
+			if animState == "dash":	animState = "runStart"
+			if animState == "idle":	animState = "runStart"
+			elif animState == "dash":	animState = "runStart"
+			elif animState == "runStop":	animState = "runStart"
+		else:
+			canDash = false
+			if animState == "runLoop":	animState = "runStop"
+		
+		if animState != "death":
+			movement = check_controller_input(movement.normalized())
+			apply_movement(movement, delta)
 
 func check_controller_input(dir):
 	var axis = Vector2(Input.get_joy_axis(0, 0), Input.get_joy_axis(0, 1))
@@ -88,92 +118,57 @@ func check_controller_input(dir):
 
 func apply_movement(direction, delta):
 	var sprite = get_node("Sprite")
+	var anim = sprite.get_animation().basename()
 	var moved = ( direction.length() != 0 )
 	
-	if moved and not self.moved:
-		if direction.y >= 1:	play_anim_beginloop("run-down")
-		elif direction.y <= -1:	play_anim_beginloop("run-up")
-		if direction.x >= 1:
-			sprite.set_flip_h(false)
-			play_anim_beginloop("run-right")
-		elif direction.x <= -1:
-			sprite.set_flip_h(true)
-			play_anim_beginloop("run-right")
-	elif moved and self.moved:
-		if direction.y >= 1:	sprite.play("run-down-loop")
-		elif direction.y <= -1:	sprite.play("run-up-loop")
-		if direction.x >= 1:
-			sprite.set_flip_h(false)
-			sprite.play("run-right-loop")
-		elif direction.x <= -1:
-			sprite.set_flip_h(true)
-			sprite.play("run-right-loop")
+	if animState == "runStart":
+		if anim != ("run-"+moveDir+"-begin"):
+			sprite.play("run-"+moveDir+"-begin")
+	elif animState == "runLoop":
+		if anim != ("run-"+moveDir+"-loop"):
+			sprite.play("run-"+moveDir+"-loop")
+	elif animState == "runStop":
+		if anim != ("run-"+moveDir+"-stop") and anim != "idle":
+			get_node("timer_idle").start()
+			sprite.play("run-"+moveDir+"-stop")
 	
-	self.moved = moved
-	if self.moved:
+	if moved:
 		var remaining = self.move(direction * movement_speed * delta)
 		if self.is_colliding():
 			var normal = self.get_collision_normal()
 			remaining = remaining.slide(normal)
 			self.move(remaining)
-#		self.move_and_slide(direction * movement_speed)# * delta)
-	else: play_anim_stop()
-
-func play_anim_beginloop(anim, only_if_moved = true):
-	var sprite = get_node("Sprite")
-	sprite.play(anim + "-begin")
-	
-	yield(sprite, "finished")
-	
-	if not only_if_moved or (only_if_moved and self.moved):
-		sprite.play(anim + "-loop")
-
-func play_anim_stop():
-	var sprite = get_node("Sprite")
-	var anim_name = sprite.get_animation()
-	
-	if anim_name.ends_with("-loop"):	anim_name = anim_name.replace("-loop", "-stop")
-	elif anim_name.ends_with("-begin"):	anim_name = anim_name.replace("-begin", "-stop")
-	
-	sprite.play(anim_name)
-	yield(sprite, "finished")
-	get_node("timer_idle").start()
-
-func input_dash(input_event):
-	if not moved or not canDash: return # cannot dash if you're not walking
-	if input_event.is_action_pressed("dash") and not input_event.is_echo() and !dashing:
-		dash(self.get_travel().normalized())
-		sound.play("dash1")
 
 func dash(direction):
-	if !dashing and !dead:
-		dashing = true
+	dashing = true
+	canDash = false
+	sound.play("dash1")
+	
+	var sprite = get_node("Sprite")
+	
+	var anim = "dash-"+moveDir+"-loop"
+	sprite.play(anim)
+	
+	var timer = 0.0
+	while timer < dash_duration:
+		var delta = get_process_delta_time()
+		self.move(direction * dash_speed * get_process_delta_time())
 		
-		var sprite = get_node("Sprite")
-		
-		var anim = sprite.get_animation().replace("run-", "dash-")
-		anim = anim.replace("-loop", "").replace("-begin", "")
-		anim = anim+"-loop"
+		timer += delta
+		yield(get_tree(), "idle_frame")
+	
+	if grounded:
+		anim = anim.replace("-loop", "-stop")
 		sprite.play(anim)
-		
-		var timer = 0.0
-		while timer < dash_duration:
-			var delta = get_process_delta_time()
-			self.move(direction * dash_speed * get_process_delta_time())
-			
-			timer += delta
-			yield(get_tree(), "idle_frame")
-		
-		play_anim_stop()
-		dashing = false
-		
-		anim = anim.replace("-loop", "-end")
-		sprite.play("anim")
-		yield(sprite, "finished")
-		
-		lastDash = dash_interval
-		#canDash = false
-		canDash = true
+		anim += "-end"
+#		sprite.play(anim)
+		get_node("timer_idle").start()
+	
+	lastDash = dash_interval
+	canDash = false
+	can_move = false
+	
+	dashing = false
 
 func advertise_colors():
 	if colors_learned & 2:	emit_signal("new_color_learned", 2)	# red
@@ -199,23 +194,22 @@ func update_checkpoint(pos):
 func death():
 	var sprite = get_node("Sprite")
 	if not dead:
+		dead = true
+		canDash = false
 		var anim_name = sprite.get_animation()
 		
-		if anim_name.ends_with("-loop"):	anim_name = anim_name.replace("-loop", "")
-		elif anim_name.ends_with("-begin"):	anim_name = anim_name.replace("-begin", "")
-		elif anim_name.ends_with("-stop"):	anim_name = anim_name.replace("-stop", "")
-		if anim_name.begins_with("run"):	anim_name = anim_name.replace("run", "death")
-		elif anim_name.begins_with("dash"):	anim_name = anim_name.replace("dash", "death")
-		if anim_name == "idle":				anim_name = "death-down"
+		if anim_name == "idle":	anim_name = "death-down"
+		else:					anim_name = "death-"+moveDir
 		sprite.play(anim_name)
 		print(anim_name)
 		
-		dead = true
 		bufferY = get_pos().y
 	else:
 		sprite.play("run-down-stop")
 		self.set_global_pos(lastCheckpoint)
 		emit_signal("death")
+		canDash = true
+		can_move = true
 		dead = false
 
 func add_coin(amount):
