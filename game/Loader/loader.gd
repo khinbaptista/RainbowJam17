f767ccbddc8148bcbb3d14961488b82e136d7053
Khin Baptista <khin.baptista@gmail.com>
2017-10-22 20:47:28 -0200

    Added multithreaded background loading

--- game/Loader/loader.gd
+++ game/Loader/loader.gd
@@ -5,8 +5,25 @@ onready var anim = get_node("AnimationPlayer")
 func change_scene( path ):
 	anim.play("load_begin")
 	yield(anim, "finished")
-	
-#	var new_scene = load(path)
-	get_tree().change_scene(path)
-	
+
+#	get_tree().change_scene(path)
+
+	# testing new multi-threaded loader
+	var loader = get_node("multithread")
+	loader.queue_resource(path)
+
+	var timer = Timer.new()
+	timer.set_autostart(true)
+	timer.set_one_shot(false)
+	timer.set_wait_time(0.005)
+	add_child(timer)
+
+	while not loader.is_ready(path):
+		var progress = "Progress: " + loader.get_progress_string(path)
+		get_node("/root/Debug").get_node("Label").set_text(progress)
+		yield(timer, "timeout")
+
+	get_node("/root/Debug").get_node("Label").set_text("")
+	get_tree().change_scene_to(loader.get_resource(path))
+
 	anim.play("load_finished")
--- game/Menu/start_menu.gd
+++ game/Menu/start_menu.gd
@@ -11,24 +11,24 @@ var music
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	#Input.warp_mouse_pos(Vector2(10000, 10000))
-	
+
 	button_start.connect("pressed", self, "pressed_start")
 	button_quit.connect("pressed", self, "pressed_quit")
-	
+
 	button_start.connect("mouse_enter", self, "changed_focus", [button_start])
 	button_quit.connect("mouse_enter", self, "changed_focus", [button_quit])
-	
+
 	changed_focus(button_start)
-	
+
 	set_process_input(true)
 
 func _input(event):
 	if event.is_action_pressed("ui_up") and not event.is_echo():
 		changed_focus(button_start)
-	
+
 	elif event.is_action_pressed("ui_down") and not event.is_echo():
 		changed_focus(button_quit)
-	
+
 	elif event.is_action_pressed("ui_accept") and not event.is_echo():
 		focused_button.emit_signal("pressed")
 
@@ -36,7 +36,7 @@ func changed_focus(button):
 	Input.start_joy_vibration(0, 0.4, 0.4, 0.1)
 	if focused_button != null:
 		focused_button.get_node("focus").hide()
-	
+
 	focused_button = button
 	focused_button.get_node("focus").show()
 
@@ -44,7 +44,6 @@ func pressed_start():
 	Input.start_joy_vibration(0, 0.4, 0.4, 0.1)
 	print("Start")
 	get_node("/root/loader").change_scene(next_scene)
-#	get_tree().change_scene_to(next_scene)
 
 func pressed_quit():
 	print("Quit")
