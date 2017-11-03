0acf4af769a25efd2021841848201365d4b16794
Khin Baptista <khin.baptista@gmail.com>
2017-11-03 14:57:04 -0200

    Controllable dash (kinda weird)

--- game/Character/actions/action_dash.gd
+++ game/Character/actions/action_dash.gd
@@ -6,6 +6,8 @@ export(float) var dash_duration	= 0.3
 export(float) var speed_bonus = 0
 export(float) var speed_multiplier = 1
 
+export(float) var max_time = 0.00
+
 onready var cooldown = get_node("cooldown")
 
 signal done
@@ -25,6 +27,9 @@ func _dash(direction):
 	var timer = 0.0
 
 	while timer < dash_duration:
+		if timer < max_time and not Input.is_action_pressed("dash"):
+			break
+
 		var speed = get_final_speed()
 		var delta = get_process_delta_time()
 
