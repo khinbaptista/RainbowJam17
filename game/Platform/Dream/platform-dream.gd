87272df89a175a0956fddf846b5171c542498273
Marcelo Vasques <msouvas@gmail.com>
2017-11-10 15:19:29 -0200

    Not working yet

--- game/Platform/Dream/platform-dream.gd
+++ game/Platform/Dream/platform-dream.gd
@@ -3,11 +3,13 @@ tool
 extends "res://Beams/color_alignment.gd"
 
 export var can_move = false
+export var affected_by_beam = false
 export(int, "red", "orange", "yellow", "green", "blue", "purple") var type = 0
 
 onready var player
 
 var active = false
+var sprite
 
 func _ready():
 	if can_move:
@@ -15,20 +17,27 @@ func _ready():
 		get_node("AnimationPlayer").seek(0)
 	elif not get_tree().is_editor_hint():
 		get_node("player_trigger").queue_free()
+		
+	if affected_by_beam:
+		set_collision_mask_bit(0, false)
+		set_layer_mask_bit(0, false)
 	
 	_hide()
 	if type == 0:
-		get_node("sprite/red").show()
+		sprite = get_node("sprite/red")
 	elif type == 1:
-		get_node("sprite/orange").show()
+		sprite = get_node("sprite/orange")
 	elif type == 2:
-		get_node("sprite/yellow").show()
+		sprite = get_node("sprite/yellow")
 	elif type == 3:
-		get_node("sprite/green").show()
+		sprite = get_node("sprite/green")
 	elif type == 4:
-		get_node("sprite/blue").show()
+		sprite = get_node("sprite/blue")
 	elif type == 5:
-		get_node("sprite/purple").show()
+		sprite = get_node("sprite/purple")
+	if not affected_by_beam:
+		sprite.show()
+	sprite.set_light_mask(color_mask)
 	#---------------------- EDITOR ONLY ----------------------
 	if get_tree().is_editor_hint():
 		set_process(true)
@@ -57,6 +66,13 @@ func _process(delta):
 		elif type == 5:
 			get_node("sprite/purple").show()
 	#---------------------------------------------------------
+	
+func spawn():
+	if sprite:
+		sprite.show()
+
+	set_collision_mask_bit(0, true)
+	set_layer_mask_bit(0, true)
 
 func _hide():
 	get_node("sprite/red").hide()
@@ -65,6 +81,16 @@ func _hide():
 	get_node("sprite/green").hide()
 	get_node("sprite/blue").hide()
 	get_node("sprite/purple").hide()
+	
+func _on_platform_area_enter( area ):
+	if color_alignment != 0:	# has a color
+		if area.get_name() == "beams_coming":
+			sprite.show()
+
+func _on_platform_area_exit( area ):
+	if color_alignment != 0:	# has a color
+		if area.get_name() == "beams_coming":
+			sprite.hide()
 
 
 func _on_player_trigger_body_enter( body ):
