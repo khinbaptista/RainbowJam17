ed4736b2e4904cb036989b86522c0ab3c07c4b6d
Marcelo Vasques <msouvas@gmail.com>
2017-12-06 16:19:11 -0200

    Seems to now work

--- game/Character/actions/action_haste.gd
+++ game/Character/actions/action_haste.gd
@@ -1,6 +1,6 @@
 extends "action.gd"
 
-export(float) var haste_ratio = 1.5
+export(float) var haste_ratio = 5
 export(float) var haste_duration = 2.0
 onready var player_move = player.get_node("Actions/move")
 onready var cooldown = get_node("cooldown")
@@ -12,7 +12,7 @@ signal done
 func can_execute():
 	return cooldown.get_time_left() == 0 and .can_execute()
 
-func _execute():
+func _execute(params):
 	if not can_execute():
 		return
 	
