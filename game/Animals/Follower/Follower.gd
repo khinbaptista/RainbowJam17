4b091bbe7b84f5113f191c943a025fa1de402f63
Marcelo Vasques <msouvas@gmail.com>
2018-01-14 15:42:42 -0200

    Follower follows and has a sight

---
+++ game/Animals/Follower/Follower.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+export(NodePath) var target_path = @"../player"
+export(int) var speed = 1
+export(bool) var active = false
+
+var target
+var final_pos
+onready var sight_area = get_node("sight")
+
+func _ready():
+	if target_path:
+		target = get_node(target_path)
+	set_process(true)
+	
+func _process(delta):
+	if sight_area.overlaps_body(target):
+		active = true
+	else:
+		active = false
+		
+	if active and target:
+		final_pos = target.get_pos()
+		var direction = final_pos - get_pos()
+		var remaining = move(direction * speed * delta)
+		if is_colliding():
+			var normal = get_collision_normal()
+			remaining = remaining.slide(normal)
+			move(remaining)
