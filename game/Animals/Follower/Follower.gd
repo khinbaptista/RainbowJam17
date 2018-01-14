76a419f6c0212256317d24eb3839fdc3e2a85917
Marcelo Vasques <msouvas@gmail.com>
2018-01-14 18:55:14 -0200

    Not working cause not getting overlapping areas

--- game/Animals/Follower/Follower.gd
+++ game/Animals/Follower/Follower.gd
@@ -7,6 +7,10 @@ export(bool) var active = false
 var target
 var final_pos
 var grounded = true
+onready var up_area = get_node("front")
+onready var down_area = get_node("back")
+onready var right_area = get_node("right")
+onready var left_area = get_node("left")
 onready var sight_area = get_node("sight")
 
 func _ready():
@@ -20,11 +24,46 @@ func _process(delta):
 	else:
 		active = false
 
-	if active and target and grounded:
-		final_pos = target.get_pos()
-		var direction = final_pos - get_pos()
-		var remaining = move(direction * speed * delta)
-		if is_colliding():
-			var normal = get_collision_normal()
-			remaining = remaining.slide(normal)
-			move(remaining)
+	if active and target:
+		if can_walk():
+			final_pos = target.get_pos()
+			var direction = final_pos - get_pos()
+			var remaining = move(direction * speed * delta)
+			if is_colliding():
+				var normal = get_collision_normal()
+				remaining = remaining.slide(normal)
+				move(remaining)
+			
+func can_walk():
+	if not target: return false
+	
+	var can = false
+	var direction = target.get_pos() - get_pos()
+	
+	if abs(direction.x) > abs(direction.y):
+		if direction.x > 0:
+			can = check_area(right_area)
+			#print("R")
+		else:
+			can = check_area(left_area)
+			#print("L")
+	else:
+		if direction.y > 0:
+			can = check_area(down_area)
+			#print("D")
+		else:
+			can = check_area(up_area)
+			#print("U")
+	return can
+	
+func check_area(area):
+	var areas = []
+	areas = area.get_overlapping_areas()
+	var can_go = false
+	
+	for a in areas:
+		if a.is_in_group("ground"):
+			can_go = true
+		elif a.is_in_group("platform"):
+			can_go = true
+	return can_go
--- game/Animals/Follower/grounded.gd
+++ game/Animals/Follower/grounded.gd
@@ -1,6 +1,11 @@
 extends Area2D
 
 onready var follower = get_parent()
+onready var up_area = get_node("../front")
+onready var down_area = get_node("../back")
+onready var right_area = get_node("../right")
+onready var left_area = get_node("../left")
+
 var found_ground
 
 func _ready():
@@ -8,18 +13,56 @@ func _ready():
 
 func _process(delta):
 	if follower.active:
-		var overlap = get_overlapping_areas()
-		found_ground = false
-		
-		for area in overlap:
-			if area.is_in_group("ground"):
-				found_ground = true
-			elif area.is_in_group("platform"):
-				found_ground = true
-			else:
-				found_ground = false or found_ground
-		
-		if found_ground:
-			follower.grounded = true
+		pass
+		#can_walk()
+		#if can_walk():
+			#var overlap = get_overlapping_areas()
+			#found_ground = false
+			
+			#for area in overlap:
+				#if area.is_in_group("ground"):
+					#found_ground = true
+				#elif area.is_in_group("platform"):
+					#found_ground = true
+				#else:
+					#found_ground = false or found_ground
+			
+			#if found_ground:
+				#follower.grounded = true
+			#else:
+				#follower.grounded = false
+				
+func can_walk():
+	if not follower.target: return false
+	
+	var can = false
+	var direction = follower.target.get_pos() - follower.get_pos()
+	
+	if abs(direction.x) > abs(direction.y):
+		if direction.x > 0:
+			can = check_area(right_area)
+			#print("R")
 		else:
-			follower.grounded = false
+			can = check_area(left_area)
+			#print("L")
+	else:
+		if direction.y > 0:
+			can = check_area(down_area)
+			#print("D")
+		else:
+			can = check_area(up_area)
+			#print("U")
+			
+	follower.can_walk = can
+	return can
+	
+func check_area(area):
+	var areas = area.get_overlapping_areas()
+	var can_go = false
+	
+	for a in areas:
+		if a.is_in_group("ground"):
+			can_go = true
+		elif a.is_in_group("platform"):
+			can_go = true
+	return can_go
