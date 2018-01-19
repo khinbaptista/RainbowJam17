0fd4cea0384186ad42ce1bd769972dd4531a5823
Khin Baptista <khin.baptista@gmail.com>
2018-01-19 06:39:24 -0200

    Added game progress node

---
+++ game/Tools/GameProgress.gd
@@ -0,0 +1,30 @@
+extends Node
+
+var player
+#export(int, FLAGS, "None", "Red", "Orange", "Yellow", "Green", "Blue", "Purple")
+var colors_learned = 1
+
+var levels = StringArray()
+var current_level = 0
+
+func _ready():
+	levels.append("res://Levels/plaza/plaza.tscn")
+	levels.append("res://Levels/world_new.tscn")
+	levels.append("res://Levels/orange/02-orange.tscn")
+	#levels.append("res://Levels/yellow/")
+	#levels.append("res://Levels/green/")
+	levels.append("res://Levels/blue/blue trailer.tscn")
+	##levels.append("res://Levels/purple/")
+
+func advance_level():
+	current_level = (current_level + 1) % levels.size()
+	get_node("/root/loader").change_scene(levels[current_level])
+
+func set_player(p):
+	player = p
+	player.colors_learned = colors_learned
+	player.connect("new_color_learned", self, "on_new_color")
+
+func on_new_color(color):
+	if colors_learned & color: return	# color is aleady known
+	colors_learned += color
