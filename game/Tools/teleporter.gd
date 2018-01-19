cc5dee47761c4b6d3006b4aa8cbd4df30413a32c
Marcelo Vasques <msouvas@gmail.com>
2018-01-19 06:47:22 -0200

    Fix red level

--- game/Tools/teleporter.gd
+++ game/Tools/teleporter.gd
@@ -1,9 +1,21 @@
 extends Node2D
 
 export(String, FILE, "*.tscn") var nextScene
+export(bool) var active = false
 
+onready var player = get_node(Globals.get("player_path"))
+
+func _ready():
+	if not active:
+		hide()
+	set_process(true)
+	player.connect("new_color_learned", self, "activate")
+	
+func activate():
+	active = true
+	show()
 
 func _on_Area2D_body_enter( body ):
-	if body.is_in_group("player"):
+	if active and body.is_in_group("player"):
 		get_node("/root/loader").change_scene(nextScene)
 		
